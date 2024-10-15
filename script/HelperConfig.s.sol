2a7f9cdf35aecbe96c5100dd6f782ffd1c8a434b
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-10-15 16:26:11 -0500

    12 11:40

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -12,6 +14,11 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2400e8;
+    int256 public constant BTC_USD_PRICE = 66000e8;
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     NetworkConfig public activeNetworkConfig;
 
     function getSepoliaNetworkConfig() public view returns (NetworkConfig memory) {
@@ -20,9 +27,27 @@ contract HelperConfig is Script {
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
-            deployerKey: vm.envUint("PRIVATE_KEY")
+            deployerKey: DEFAULT_ANVIL_KEY
         });
     }
 
-    function getOrCreateAnvilNetworkConfig() public view returns (NetworkConfig memory) {}
+    function getOrCreateAnvilNetworkConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator wethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        MockV3Aggregator wbtcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
+        ERC20Mock weth = new ERC20Mock("Wrapped Ether", "WETH", msg.sender, 1000e8);
+        ERC20Mock wbtc = new ERC20Mock("Wrapped Bitcoin", "WBTC", msg.sender, 1000e8);
+        vm.stopBroadcast();
+        return NetworkConfig({
+            wethUsdPriceFeed: address(wethUsdPriceFeed),
+            wbtcUsdPriceFeed: address(wbtcUsdPriceFeed),
+            weth: address(weth),
+            wbtc: address(wbtc),
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
 }
