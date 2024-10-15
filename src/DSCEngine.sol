9c5790f95a3e33a8b0edd2406897e5ea7b6b6cc5
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-10-15 03:48:47 -0500

    complete 06 DSCEngine setup

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -37,10 +37,28 @@ pragma solidity ^0.8.18 ;
  * -Algorithmically Stable 
  * It is similar to DAI if DAI had no governence, no fees, and was only backed by WETH and WBTC
  * 
+ * Our DSC system should always be overcollateralized. At no point, should the value of all collateral be less than the USD value of all the DSC. 
+ * 
  * @notice This contract is the core of the DSC system. It handles all the logid of mining and redeeming DSC as well as depositing and withdrawing collateral.
  * @notice This contract is VERY loosely based on the MakerDAO DSS (DAI) system. 
  */
 
 contract DSCEngine {
+
+    function depositCollateralAndMintDSC() external {}
+
+    function depositCollateral() external{}
+
+    function redeemCollateralForDSC() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDSC() external {}
+
+    function burnDSC() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
 
 }
