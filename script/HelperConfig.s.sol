30d672458911011015d0f97bf2fef7b2c7311d0b
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-10-16 02:26:11 -0500

    13 4:26 test fail

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -17,8 +17,7 @@ contract HelperConfig is Script {
     uint8 public constant DECIMALS = 8;
     int256 public constant ETH_USD_PRICE = 2400e8;
     int256 public constant BTC_USD_PRICE = 66000e8;
-    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
@@ -29,13 +28,13 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaNetworkConfig() public pure returns (NetworkConfig memory) {
+    function getSepoliaNetworkConfig() public view returns (NetworkConfig memory) {
         return NetworkConfig({
             wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
-            deployerKey: DEFAULT_ANVIL_KEY
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
         });
     }
 
@@ -55,7 +54,7 @@ contract HelperConfig is Script {
             wbtcUsdPriceFeed: address(wbtcUsdPriceFeed),
             weth: address(weth),
             wbtc: address(wbtc),
-            deployerKey: vm.envUint("PRIVATE_KEY")
+            deployerKey: vm.envUint("DEFAULT_ANVIL_PRIVATE_KEY")
         });
     }
 }
--- test/DSCEngineTest.t.sol
+++ test/DSCEngineTest.t.sol
@@ -5,14 +5,31 @@ import {Test} from "forge-std/Test.sol";
 import {DeployDSC} from "script/DeployDSC.s.sol";
 import {DSCEngine} from "src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
     DSCEngine dscengine;
     DecentralizedStableCoin dsc;
+    HelperConfig config;
+
+    address ethUsdPriceFeed;
+    address weth;
 
     function setUp() public {
         deployer = new DeployDSC();
-        (dsc, dscengine) = deployer.run();
+        (dsc, dscengine, config) = deployer.run();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+    }
+
+    /// Price Tests ///
+
+    function testGetUSDValue() public view {
+        // test the simple math, 10 eth * 2400 usd = 24000 usd
+        uint256 ethAmount = 10e18;
+        uint256 expectedUsdValue = 24000e18;
+
+        uint256 actualUsdValue = dscengine.getUSDValue(weth, ethAmount);
+        assertEq(actualUsdValue, expectedUsdValue);
     }
 }
