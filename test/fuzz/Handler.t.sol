2902dcd818f485b25e86c220adbf43e0beb237e2
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-04-09 09:12:24 -0500

    22 3:00

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,2 +1,23 @@
 // SPDX-License-Identifier: MIT
+// Handler is going to narrow down the way we call functions 
+
 pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+
+contract Handler is Test {
+
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    constructor(DSCEngine _dsce, DecentralizedStableCoin _dsc) {
+        dsce = _dsce;
+        dsc = _dsc;
+
+    }
+
+    // redeem collateral only when there is collateral
+
+}
--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,51 +1,51 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
 
-// Have our invariants aka properties
+// // Have our invariants aka properties
 
-// What are our invariants?
+// // What are our invariants?
 
-// 1. Total supply of DSC must be less than the total value of all collateral in USD
+// // 1. Total supply of DSC must be less than the total value of all collateral in USD
 
-// 2. Getter-view functions should never revert
+// // 2. Getter-view functions should never revert <--- evergreen invariant 
 
-pragma solidity ^0.8.18;
+// pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
-import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {DeployDSC} from "script/DeployDSC.s.sol";
-import {DSCEngine} from "src/DSCEngine.sol";
-import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
-import {HelperConfig} from "script/HelperConfig.s.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {Test} from "forge-std/Test.sol";
+// import {StdInvariant} from "forge-std/StdInvariant.sol";
+// import {DeployDSC} from "script/DeployDSC.s.sol";
+// import {DSCEngine} from "src/DSCEngine.sol";
+// import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+// import {HelperConfig} from "script/HelperConfig.s.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract InvariantsTest is StdInvariant, Test {
-    DeployDSC deployer;
-    DSCEngine dsce;
-    DecentralizedStableCoin dsc;
-    HelperConfig config;
-    address weth;
-    address wbtc;
+// contract InvariantsTest is StdInvariant, Test {
+//     DeployDSC deployer;
+//     DSCEngine dsce;
+//     DecentralizedStableCoin dsc;
+//     HelperConfig config;
+//     address weth;
+//     address wbtc;
 
-    function setUp() external {
-        deployer = new DeployDSC();
+//     function setUp() external {
+//         deployer = new DeployDSC();
 
-        (dsc, dsce, config) = deployer.run();
-        (,, weth, wbtc,) = config.activeNetworkConfig();
+//         (dsc, dsce, config) = deployer.run();
+//         (,, weth, wbtc,) = config.activeNetworkConfig();
 
-        targetContract(address(dsce));
-    }
+//         targetContract(address(dsce));
+//     }
 
-    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
-        // get the value of all the collateral in the protocol
-        // compare that to all the dsc (debt)
+//     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+//         // get the value of all the collateral in the protocol
+//         // compare that to all the dsc (debt)
 
-        uint256 totalSupply = dsc.totalSupply();
-        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
-        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+//         uint256 totalSupply = dsc.totalSupply();
+//         uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+//         uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
 
-        uint256 wethValue = dsce.getUSDValue(weth, totalWethDeposited);
-        uint256 wbtcValue = dsce.getUSDValue(wbtc, totalWbtcDeposited);
+//         uint256 wethValue = dsce.getUSDValue(weth, totalWethDeposited);
+//         uint256 wbtcValue = dsce.getUSDValue(wbtc, totalWbtcDeposited);
 
-        assert(wethValue + wbtcValue >= totalSupply);
-    }
-}
+//         assert(wethValue + wbtcValue >= totalSupply);
+//     }
+// }
