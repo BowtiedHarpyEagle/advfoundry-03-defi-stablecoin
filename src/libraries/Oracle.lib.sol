704fd85c1dd3ed950507321dcf173b71833c5900
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-04-18 15:02:31 -0500

    27 6:30 finish oracle lib

--- src/libraries/Oracle.lib.sol
+++ src/libraries/Oracle.lib.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
 /*
  * @title OracleLib
  * @author Bowtied HarpyEagle - Based on Updraft Course
@@ -14,4 +17,26 @@ pragma solidity ^0.8.18;
  * This is a known issue with Chainlink.
  */
 
-library OracleLib {}
+library OracleLib {
+
+    error OracleLib__PriceIsStale();
+
+    uint256 private constant TIMEOUT = 3 hours; // 3 hours in seconds = 10800 seconds
+    
+    function  staleCheckLatestRoundData(AggregatorV3Interface priceFeed) 
+    public view  
+    returns (uint80, int256, uint256, uint256, uint80) {
+
+        (uint80 roundId, int256 answer, uint256 startedAt, 
+        uint256 updatedAt, uint80 answeredInRound) = 
+        priceFeed.latestRoundData();
+
+        uint256 secondsSinceUpdate = block.timestamp - updatedAt;
+        if (secondsSinceUpdate > TIMEOUT) {
+            revert OracleLib__PriceIsStale();
+        }
+
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+        
+    }
+}
